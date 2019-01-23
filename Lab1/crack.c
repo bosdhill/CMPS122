14eb50e9668c4bc241192034b7b6ff90c89b11e5
Bobby Singh Dhillon <bosdhill@grunhilda.soe.ucsc.edu>
2019-01-22 21:23:17 -0800

    works but has debug statements

--- Lab1/crack.c
+++ Lab1/crack.c
@@ -11,27 +11,31 @@
 #include <string.h>
 #include <unistd.h>
 #define KEYSPACESIZE 62
-#define MAXLEN 1024
-#define NUMTHREADS 20
+#define NUMTHREADS 24
 #define NONE "0"
 #define CPUAFFINITY 0
+#define PWLEN 4
+#define HASHLEN 13
+#define MAXLEN 20
+#define NUMUSERS 6
 
 struct args_t {
     int lower;
     int upper;
-    char username[MAXLEN];
-    char cryptPasswd[MAXLEN];
-    char passwd[MAXLEN];
+    char salt[2];
+    char *cryptPasswd;
+    char *passwd;
 };
 
 char keySpace[KEYSPACESIZE] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
 int cracked = 0;
-
+char crackedPassword[5];
 void crackStealthy(char *, char *, int, char *, int);
+
 /*
  * Parse file in path FNAME into USERS and PASSWDS, returning the COUNT of both.
  */
-void parseFile(char *fname, char users[][MAXLEN], char passwds[][MAXLEN], int *count) {
+void parseFile(char *fname, char users[][MAXLEN], char passwds[][HASHLEN + 1], int *count) {
     FILE *passwdStream = fopen(fname, "r");
     *count = 0;
     char *line = NULL;
@@ -63,8 +67,8 @@ void crackSingle(char *username, char *cryptPasswd, int pwlen, char *passwd) {
  */
 void crackMultiple(char *fname, int pwlen, char **passwds) {
     int size;
-    char users[MAXLEN][MAXLEN];
-    char usersCryptPasswds[MAXLEN][MAXLEN];
+    char users[NUMUSERS][MAXLEN];
+    char usersCryptPasswds[NUMUSERS][HASHLEN + 1];
     parseFile(fname, users, usersCryptPasswds , &size);
     for (int i = 0; i < size; i++) {
         // printf("users[%d] = %s, usersCryptPasswds[%d] = %s\n", i, users[i], i, usersCryptPasswds[i]);
@@ -81,21 +85,17 @@ void crackSpeedy(char *fname, int pwlen, char **passwds) {
     crackMultiple(fname, pwlen, passwds);
 }
 
-void printArgs(int num, int lower, int upper, char *username,
+void printArgs(pthread_t num, long unsigned int lower, long unsigned int upper, char salt[2],
     char *cryptPasswd, char *passwd) {
-        printf("round %d args: %d %d %s %s %s\n", num, lower, upper, username,
-            cryptPasswd, passwd);
+        printf("\t\tth: %lu\n\t\tlower=%lu\n\t\tupper=%lu\n\t\tsalt=%c%c\n\t\thash=%s\n\n",
+            num, lower, upper, salt[0], salt[1], cryptPasswd);
 }
 
-void crackSingleReentrant(int lower, int upper, char username[],
-    char cryptPasswd[], char *passwd) {
-    char salt[] = {username[0], username[1]};
+void crackSingleReentrant(int lower, int upper, char salt[2],
+    char *cryptPasswd, char *passwd) {
     char *cryptRandomPasswd;
     struct crypt_data data;
     data.initialized = 0;
-
-    // if (lower == 54 || upper == 57)
-    //     printArgs(2, lower, upper, username, cryptPasswd, passwd);
 
     for (int i = lower; i < upper && !cracked; i++) {
         for (int j = 0; j < KEYSPACESIZE && !cracked; j++) {
@@ -105,10 +105,10 @@ void crackSingleReentrant(int lower, int upper, char username[],
                                             keySpace[k], keySpace[w], '\0'};
 
                     cryptRandomPasswd = crypt_r(randomPasswd, salt, &data);
+
                     if (strcmp(cryptRandomPasswd, cryptPasswd) == 0) {
-                        strcpy(passwd, randomPasswd);
-                        // printf("found with id = %lu\n", pthread_self());
-                        // printArgs(3, lower, upper, username, cryptPasswd, passwd);
+                        strcpy(crackedPassword, randomPasswd);
+                        printf("crypted! = %s\n", crackedPassword);
                         cracked = 1;
                         break;
                     }
@@ -119,36 +119,21 @@ void crackSingleReentrant(int lower, int upper, char username[],
 }
 
 void *crackMultiThreaded(void *vargp) {
-    struct args_t *args = (struct args_t *)vargp;
-    // if (args->lower == 54)
-    //     printArgs(1, args->lower, args->upper, args->username,
-    //         args->cryptPasswd, args->passwd);
-    // if (args->lower == 57)
-    //     printArgs(11, args->lower, args->upper, args->username,
-    //         args->cryptPasswd, args->passwd);
-
-    crackSingleReentrant(args->lower, args->upper, args->username,
-        args->cryptPasswd, args->passwd);
-    if (strcmp(args->passwd, NONE) != 0)
-        return (void *)args->passwd;
+    struct args_t *args = vargp;
+    printArgs(pthread_self(), args->lower, args->upper, args->salt, args->cryptPasswd, args->passwd);
+    crackSingleReentrant(args->lower, args->upper, args->salt, args->cryptPasswd, args->passwd);
     return NULL;
 }
 
 void createThread(pthread_t *thread, int lower, int upper, int cpu,
-    char username[], char cryptPasswd[]) {
-    struct args_t args;
-    args.lower = lower;
-    args.upper = upper;
-    strcpy(args.username, username);
-    strcpy(args.cryptPasswd, cryptPasswd);
-    strcpy(args.passwd, NONE);
+    char salt[2], char *cryptPasswd) {
+    struct args_t *args = (struct args_t*)malloc(sizeof(struct args_t));
+    args->lower = lower;
+    args->upper = upper;
+    memcpy(args->salt, salt, 2);
+    args->cryptPasswd = cryptPasswd;
 
-    // if (lower == 54)
-    //     printArgs(0, lower, upper, args.username, args.cryptPasswd, args.passwd);
-    // if (lower == 57)
-    //     printArgs(10, lower, upper, args.username, args.cryptPasswd, args.passwd);
-
-    int iret = pthread_create(thread, NULL, crackMultiThreaded, &args);
+    int iret = pthread_create(thread, NULL, crackMultiThreaded, (void *)args);
     if (iret) perror("createThread: thread could not be created");
 
     if (CPUAFFINITY) {
@@ -162,46 +147,33 @@ void createThread(pthread_t *thread, int lower, int upper, int cpu,
     }
 }
 
-void createThreads(pthread_t threads[], char *username, char *cryptPasswd) {
+void createThreads(pthread_t threads[], char salt[2], char *cryptPasswd) {
     int offset = KEYSPACESIZE / NUMTHREADS;
     int from;
     int upto;
-    char args_username[MAXLEN];
-    char args_cryptPasswd[MAXLEN];
-
+    printf("createThreads\n");
     for (int i = 0; i < NUMTHREADS - 1; i++) {
         from = offset*i;
         upto = offset*(i+1);
-        // printf("from %d to %d\n", from, upto);
-        strcpy(args_username, username);
-        strcpy(args_cryptPasswd, cryptPasswd);
-        createThread(&threads[i], from, upto, i, args_username,
-            args_cryptPasswd);
-        memset(args_username, '\0', sizeof(args_username));
-        memset(args_cryptPasswd, '\0', sizeof(args_username));
+        printf("\tfrom=%d to=%d\n", from, upto);
+        if (cracked) return;
+        createThread(&threads[i], from, upto, i, salt, cryptPasswd);
     }
     from = offset * (NUMTHREADS - 1);
     upto = KEYSPACESIZE;
-    // printf("from %d to %d\n", from, upto);
-    strcpy(args_username, username);
-    strcpy(args_cryptPasswd, cryptPasswd);
-    createThread(&threads[NUMTHREADS - 1], from, upto, NUMTHREADS - 1, args_username,
-        args_cryptPasswd);
+    printf("\tfrom=%d to=%d\n", from, upto);
+    if (cracked) return;
+    createThread(&threads[NUMTHREADS - 1], from, upto, NUMTHREADS - 1, salt,
+        cryptPasswd);
 }
 
-void joinThreads(pthread_t threads[], char *passwd) {
+void joinThreads(pthread_t threads[]) {
     int iret = 0;
-    void *ret = NULL;
     for (int i = 0; i < NUMTHREADS; i++) {
-    //    printf("joining thread id = %lu\n", threads[i]);
-       iret = pthread_join(threads[i], &ret);
+       iret = pthread_join(threads[i], NULL);
        if (iret) perror("joinThreads: thread could not join");
-       if (ret != NULL) {
-           strcpy(passwd, (char *)ret);
-           break;
-       }
+       if (cracked) break;
     }
-    printf("passwd = %s\n", passwd);
 }
 
 
@@ -210,9 +182,11 @@ void joinThreads(pthread_t threads[], char *passwd) {
  * given the encrypted password CRYPTPASSWD without using more than MAXCPU
  * percent of any processor.
  */
-void crackStealthy(char *username, char *cryptPasswd, int pwlen,
-    char *passwd, int maxCpu) {
+void crackStealthy(char *username, char *cryptPasswd, int pwlen, char *passwd,
+    int maxCpu) {
     pthread_t threads[NUMTHREADS];
-    createThreads(threads, username, cryptPasswd);
-    joinThreads(threads, passwd);
+    char salt[2] = {username[0], username[1]};
+    createThreads(threads, salt, cryptPasswd);
+    joinThreads(threads);
+    strcpy(passwd, crackedPassword);
 }
