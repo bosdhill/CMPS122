150ebc3752136220ffbd4757c3bc65ecf9c54a9c
Bobby Singh Dhillon <bosdhill@grunhilda.soe.ucsc.edu>
2019-02-02 23:01:08 -0800

    got up until dictionary working and automated

--- Lab2/basic.sh
+++ Lab2/basic.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-nc -zv 192.168.1.10 1024-65535 2>&1 | tee out | grep -- "Connection to" 
+nc -zv 192.168.1.10 1024-65535 2>&1 | grep -- "Connection to" > out
+python checkports.py
---
+++ Lab2/checkports.py
@@ -0,0 +1,31 @@
+import re
+import socket
+
+HOST = '192.168.1.10'
+SKELETONKEY = 'Passepartout'
+BYTES = 1024
+CRUZID = 'bosdhill\n'
+
+def try_ports():
+     fp = open("out", "r")
+     for _, line in enumerate(fp):
+          port = re.findall("[0-9]{5}", line)
+          PORT = int(port[0])
+          s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+          s.connect((HOST, PORT))
+          s.sendall(SKELETONKEY)
+          data = s.recv(BYTES)
+          s.sendall(CRUZID)
+          data = s.recv(BYTES)
+          if data == 'Password: ':
+               print('Received %s from port %d' % (repr(data), PORT))
+
+if __name__ == "__main__":
+     try_ports()
+
+
+
+
+
+
+
