8ff0a0d8a65c318f6b68db7365bdfa0b4a1445d9
Bobby Dhillon (Contractor) <bdhillon@sjcmac2ezgg8wp.paloaltonetworks.local>
2019-02-27 18:40:40 -0800

    strncat -> strcat

--- Lab3/server/http.c
+++ Lab3/server/http.c
@@ -63,7 +63,7 @@ void getPathToFile(char *path, char file_path[]) {
 void sendFile(int sock, char *path) {
     printf("sendFile\n");
     char absolute_path[SIZE];
-    strncat(absolute_path, homedir, SIZE);
+    strcat(absolute_path, homedir);
     printf("absolute_path = %s\n", absolute_path);
     getPathToFile(path, absolute_path);
     // binary(sock, findFile(path));
