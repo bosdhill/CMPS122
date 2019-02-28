06ed1ccfd498d0bca607458321a056048e09a0ae
Bobby Dhillon (Contractor) <bdhillon@sjcmac2ezgg8wp.paloaltonetworks.local>
2019-02-27 20:40:59 -0800

    got rid of print statements and comment

--- Lab3/server/http.c
+++ Lab3/server/http.c
@@ -24,8 +24,6 @@ char BADREQ[] = "HTTP/1.1 400 Bad Request\n";
 
 
 void http_response(int sock) {
-    // char data[] = "\n";
-    // send(sock, (void *)data, strlen(data) + 1,0);
     char SUCCESS[] = "\nHTTP/1.1 200 OK\n";
     send(sock, (void *)SUCCESS, strlen(SUCCESS) + 1,0);
 }
@@ -77,7 +75,6 @@ void sendFile(int sock, char path[]) {
     strncat(absolute_file_path, homedir, strlen(homedir) + 1);
     strncat(absolute_file_path, path, strlen(path) + 1);
     printf("absolute_path = %s\n", absolute_file_path);
-    printf("strlen of absolute_path = %d\n", strlen(absolute_file_path));
     http_response(sock);
     binary(sock, absolute_file_path);
 }
@@ -120,7 +117,6 @@ void httpRequest(int sock, char *request) {
         char path[SIZE/2] = {0};
         getPathFromHttp(request, path);
         printf("path = %s\n", path);
-        printf("strlen of path = %d\n", strlen(path));
         sendFile(sock, path);
     }
 }
