dd6ea7556aceefba3d9248850145cf0205179f50
Bobby Dhillon (Contractor) <bdhillon@sjcmac2ezgg8wp.paloaltonetworks.local>
2019-02-27 22:23:30 -0800

    testing for it

--- Lab3/server/http.c
+++ Lab3/server/http.c
@@ -111,7 +111,7 @@ void get_path_from_http(char *request, char path[]) {
 
 void get_content_from_http(char *request) {
     // char delim[] = "\r\n\r\n";
-    char *content_start = strstr(request, "\r\n\r\n") + 4;
+    char *content_start = strstr(request, "\r\n\r\n");
     printf("\tcontent = %s\n", content_start);
 }
 
