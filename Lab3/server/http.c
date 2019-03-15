35c0ecf7c7920f6d1aeb854194658c4f9f5771b9
Bobby Singh Dhillon <bdhillon1994@gmail.com>
2019-03-14 19:58:35 -0700

    xor shit in here bruh

--- Lab3/server/http.c
+++ Lab3/server/http.c
@@ -15,11 +15,12 @@
 #include <errno.h>
 
 #define BYTES 2048
-#define VALID 1
-#define INVALID 0
+#define cipher_textID 1
+#define INcipher_textID 0
 #define SIZE 512
 #define USERMAX 30
 #define PASSMAX 30
+#define MAX_USERS 50
 
 enum req_type{POST, GET, NONE};
 char homedir[SIZE/2] = {0};
@@ -29,6 +30,7 @@ char BADREQ[] = "HTTP/1.1 400 Bad Request\r\n\r\n";
 char FORBIDDEN[] = "HTTP/1.1 401 Forbidden\r\n\r\n";
 char COOKIE[] = "HTTP/1.1 200 OK\r\nSet-Cookie: ";
 char CONTINUE[] = "HTTP/1.1 100-continue\r\n";
+char *keys[MAX_USERS];
 unsigned char EXPECT = 0;
 int content_length = 0;
 mode_t umask_0 = 0777;
@@ -62,6 +64,47 @@ static void binary(int sock, char *fname) {
             write(sock, buffer, bytes);
         }
    }
+}
+
+void hash(const char *user, const char *pass) {
+    int K = strlen(pass);
+    int M = strlen(user);
+    unsigned char cipher_text[M + K];
+    unsigned char key[M + K];
+    int cnt = 0;
+    printf("\nmessage\n");
+    for (int i = 0; i < M + K; i++) {
+        key[i] = (unsigned char)(1 + rand() % 100);
+    }
+    for (int i = 0; i < M; i++) {
+        printf("%d ", user[i]);
+        cipher_text[cnt] = user[i] ^ key[cnt];
+        cnt++;
+    }
+    for (int i = 0; i < K; i++) {
+        printf("%d ", pass[i]);
+        cipher_text[cnt] = pass[i] ^ key[cnt];
+        cnt++;
+    }
+    printf("\nkey\n");
+    for (int i = 0; i < M + K; i++) {
+        printf("%d ", key[i]);
+    }
+
+    printf("\ncipher text\n");
+    for (int i = 0; i < M + K; i++) {
+        printf("%d ", cipher_text[i]);
+    }
+
+    printf("\nmessage\n");
+
+    for (int i = 0; i < M + K; i++) {
+        cipher_text[i] = cipher_text[i] ^ key[i];
+    }
+
+    for (int i = 0; i < M + K; i++) {
+        printf("%c ", (unsigned char)cipher_text[i]);
+    }
 }
 
 int create_file_named(const char *fname, char content[], int sock) {
@@ -131,13 +174,13 @@ int create_directory_path_from(const char path[], char dir_path[]) {
     strncpy(orig_path, path, SIZE);
     char *delim = "/";
     char *token = strtok(orig_path, delim);
-    if (token == NULL) return INVALID;
+    if (token == NULL) return INcipher_textID;
     do {
         strncat(dir_path, token, SIZE);
         strncat(dir_path, delim, SIZE);
         mkdir(dir_path, umask_0);
     } while((token = strtok(NULL, delim)) != NULL);
-    return VALID;
+    return cipher_textID;
 }
 
 void get_path_to_file_and_filename(const char *path, char file[], char file_path[]) {
@@ -164,7 +207,7 @@ void write_file_to(int sock, const char path[], char content[]) {
     char fname[SIZE] = {0};
     char dir_path[SIZE] = {0};
     get_path_to_file_and_filename(path, fname, path_to_file);
-    if (create_directory_path_from(path_to_file, dir_path) == INVALID){
+    if (create_directory_path_from(path_to_file, dir_path) == INcipher_textID){
         send_http_response(sock, BADREQ);
         exit(-1);
     }
@@ -181,11 +224,12 @@ int get_user_pass_from_http(const char *content, char *user, char *pass) {
     char orig_content[SIZE];
     strncpy(orig_content, content, SIZE);
     char *token = strtok(orig_content, "&");
-    if (token == NULL) return INVALID;
+    if (token == NULL) return INcipher_textID;
     strncpy(user, strstr(token, "login?username=") + strlen("login?username="), USERMAX);
     token = strtok(NULL, "&");
     strncpy(pass, strstr(token, "password=") + strlen("password="), PASSMAX);
-    return VALID;
+    hash(user, pass);
+    return cipher_textID;
 }
 
 void get_user_from_path(const char *path, char user[]) {
@@ -205,11 +249,11 @@ int verify_user(const char *user, const char *pass) {
         next_pass = strtok(NULL, ":");
         next_pass[strcspn(next_pass, "\n")] = 0;
         if (strcmp(next_user, user) == 0 && strcmp(next_pass, pass) == 0) {
-            return VALID;
+            return cipher_textID;
         }
     }
     fclose(file);
-    return INVALID;
+    return INcipher_textID;
 }
 
 int verify_cookie(const char *path, const char *cookie) {
@@ -225,7 +269,7 @@ void get_path_from_http(const char *request, char path[]) {
     strcpy(path, strtok(NULL, " "));
 }
 
-int valid_path(const char *path) {
+int cipher_textid_path(const char *path) {
     char orig_path[SIZE];
     strncpy(orig_path, path, SIZE);
     return strlen(orig_path) > 1 && strstr(orig_path, "..") == NULL;
@@ -244,16 +288,16 @@ int get_cookie_from_http(const char *request, char cookie[]) {
     strncpy(orig_request, request, SIZE);
     char* begin = strstr(request, "Cookie: cookie=");
     if (begin == NULL) {
-        return INVALID;
+        return INcipher_textID;
     }
     strncpy(cookie, begin + strlen("Cookie: cookie="), PASSMAX);
     cookie[strcspn(cookie, "\r\n")] = 0;
-    return VALID;
+    return cipher_textID;
 }
 
 int verify(const char *request, const char *path, char cookie[]) {
-    return strlen(path) > 1 && get_cookie_from_http(request, cookie) == VALID
-            && verify_cookie(path, cookie) == VALID;
+    return strlen(path) > 1 && get_cookie_from_http(request, cookie) == cipher_textID
+            && verify_cookie(path, cookie) == cipher_textID;
 }
 
 enum req_type get_req_type(const char *request) {
@@ -293,7 +337,7 @@ void httpRequest(int sock, char *request) {
         char path[SIZE];
         char cookie[PASSMAX + strlen("\r\n") + 1];
         get_path_from_http(request, path);
-        if (valid_path(path) && verify(request, path, cookie)) {
+        if (cipher_textid_path(path) && verify(request, path, cookie)) {
             send_file_to(sock, path);
         }
         else
@@ -307,16 +351,16 @@ void httpRequest(int sock, char *request) {
         char content[BYTES];
 
         get_path_from_http(request, path);
-        if (valid_path(path)) {
-            if (get_cookie_from_http(request, cookie) == VALID
-                && verify_cookie(path, cookie) == VALID) {
+        if (cipher_textid_path(path)) {
+            if (get_cookie_from_http(request, cookie) == cipher_textID
+                && verify_cookie(path, cookie) == cipher_textID) {
                     get_content_from_http(request, content);
                     set_content_length(request);
                     check_expect_100(request);
                     write_file_to(sock, path, content);
             }
-            else if (get_user_pass_from_http(path, user, pass) == VALID
-                    && verify_user(user, pass) == VALID) {
+            else if (get_user_pass_from_http(path, user, pass) == cipher_textID
+                    && verify_user(user, pass) == cipher_textID) {
                     set_cookie(user, pass, cookie);
                     send_http_cookie(sock, cookie);
             }
